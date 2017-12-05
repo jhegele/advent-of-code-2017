960a321a7de697acf39ace848b6d4e38042fc863
jhegele <john.hegele@gmail.com>
2017-12-05 05:43:58 -0500

    Add Day 5

---
+++ day5/puzzle1.py
@@ -0,0 +1,12 @@
+with open('input.txt', 'r') as puzzle_input:
+    jumps = [int(line.strip()) for line in puzzle_input.readlines()]
+
+steps = 0
+curr_idx = 0
+while curr_idx < len(jumps):
+    move = jumps[curr_idx]
+    jumps[curr_idx] += 1
+    curr_idx += move
+    steps += 1
+
+print('{} steps taken'.format(steps))
